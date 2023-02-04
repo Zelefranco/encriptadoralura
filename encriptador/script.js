1af3df77b1a83417c229f56c97929aabe0935487
Zelefranco <123840765+Zelefranco@users.noreply.github.com>
2023-02-04 12:01:00 -0500

    Add files via upload
    Agregando archivos de

---
+++ encriptador/script.js
@@ -0,0 +1,34 @@
+/* getelemente esta llamando el id de html y el toLowerCase es para que convierta todas las letras en minusculas*/
+/* i= es para que tome letras mayusculas y minusculas
+    g= tome en cuenta toda la linea o la oracion 
+    m= tome en cuenta multiples lines */
+
+function encriptar(){
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/e/img, "enter");
+    var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+}
+
+function desencriptar(){
+    var frase = document.getElementById("textoEncriptado").value.toLowerCase();
+
+    var textoEncriptado = frase.replace(/enter/img, "e");
+    var textoEncriptado = textoEncriptado.replace(/ober/img, "o");
+    var textoEncriptado = textoEncriptado.replace(/imes/img, "i");
+    var textoEncriptado = textoEncriptado.replace(/ai/img, "a");
+    var textoEncriptado = textoEncriptado.replace(/ufat/img, "u");
+
+    document.getElementById("textoDesencriptado").innerHTML = textoEncriptado;
+}
+
+function copiar(){
+    var contenido = document.querySelector("#textoDesencriptado");
+    contenido.select();
+    document.execCommand("copy")
+}
